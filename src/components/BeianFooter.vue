fbeba36286fca1df040668d8da5a74a757bd189d
fimmy <fimmy@live.cn>
2021-05-09 09:17:16 +0800

    修改备案页面脚

--- src/components/BeianFooter.vue
+++ src/components/BeianFooter.vue
@@ -1,19 +1,40 @@
 <template>
   <a-layout-footer class="footer" style="text-align: center">
-    <a href="https://beian.miit.gov.cn" target="_blank">粤ICP备2021039208号-1</a>
+    <a
+      target="_blank"
+      href="http://www.beian.gov.cn/portal/registerSystemInfo?recordcode=44060402002080"
+    >
+      <img src="@/assets/备案图标.png" style="float: left" />
+      <p>粤公网安备 44060402002080号</p>
+    </a>
+    <a target="_blank" href="https://beian.miit.gov.cn">
+      <p>粤ICP备2021039208号-1</p>
+    </a>
   </a-layout-footer>
 </template>
 
 <script lang="ts">
-import { Component, Vue } from 'vue-property-decorator'
+import { Component, Vue } from 'vue-property-decorator';
 
 @Component
-export default class BeianFooter extends Vue {
-}
+export default class BeianFooter extends Vue {}
 </script>
 
 <style lang="less" scoped>
 .footer {
   background-color: #fff;
+  a {
+    display: inline-block;
+    text-decoration: none;
+    height: 20px;
+    line-height: 20px;
+    p {
+      float: left;
+      height: 20px;
+      line-height: 20px;
+      margin: 0px 0px 0px 5px;
+      color: #939393;
+    }
+  }
 }
 </style>
