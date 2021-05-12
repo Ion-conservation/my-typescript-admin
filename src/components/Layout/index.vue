809b53ee6fa7181c7b2ecf256cca766a9734d3c1
fimmy <fimmy@live.cn>
2021-05-12 13:15:40 +0800

    恢复备案页脚

--- src/components/Layout/index.vue
+++ src/components/Layout/index.vue
@@ -7,22 +7,28 @@
       <a-layout-sider theme="light">
         <side-bar />
       </a-layout-sider>
-      <a-layout-content>
-        <router-view />
-      </a-layout-content>
+      <a-layout>
+        <a-layout-content>
+          <router-view />
+        </a-layout-content>
+        <beian-footer/>
+      </a-layout>
     </a-layout>
+
   </a-layout>
 </template>
 <script lang="ts">
 import { Component, Vue } from 'vue-property-decorator';
 import NavBar from './components/NavBar.vue';
 import SideBar from './components/SideBar.vue';
+import BeianFooter from '../BeianFooter.vue'
 
 @Component({
   name: 'Layout',
   components: {
     NavBar,
     SideBar,
+    BeianFooter
   },
 })
 export default class Layout extends Vue {}
